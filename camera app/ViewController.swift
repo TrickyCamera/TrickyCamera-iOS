25c3e238c44303ef658f06b4754667d400bdc884
elzzup <guild0105@gmail.com>
2015-03-15 13:41:03 +0900

    update tricky camera

--- camera app/ViewController.swift
+++ camera app/ViewController.swift
@@ -7,19 +7,106 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    // セッション.
+    var mySession : AVCaptureSession!
+    // デバイス.
+    var myDevice : AVCaptureDevice!
+    // 画像のアウトプット.
+    var myImageOutput : AVCaptureStillImageOutput!
+    // サウンド
+    var audioPlayer:AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // セッションの作成.
+        mySession = AVCaptureSession()
+        // デバイス一覧の取得.
+        let devices = AVCaptureDevice.devices()
+
+        // バックカメラをmyDeviceに格納.
+        for device in devices{
+            if(device.position == AVCaptureDevicePosition.Back){
+                myDevice = device as AVCaptureDevice
+            }
+        }
+
+        // バックカメラからVideoInputを取得.
+        let videoInput = AVCaptureDeviceInput.deviceInputWithDevice(myDevice, error: nil) as AVCaptureDeviceInput
+
+        // セッションに追加.
+        mySession.addInput(videoInput)
+
+        // 出力先を生成.
+        myImageOutput = AVCaptureStillImageOutput()
+
+        // セッションに追加.
+        mySession.addOutput(myImageOutput)
+
+        // 画像を表示するレイヤーを生成.
+        let myVideoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(mySession) as AVCaptureVideoPreviewLayer
+        myVideoLayer.frame = self.view.bounds
+        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+
+        // Viewに追加.
+        self.view.layer.addSublayer(myVideoLayer)
+
+        // セッション開始.
+        mySession.startRunning()
+
+        // UIボタンを作成.
+        let myButton = UIButton(frame: CGRectMake(0,0,120,50))
+
+        myButton.backgroundColor = UIColor.redColor();
+        myButton.layer.masksToBounds = true
+        myButton.setTitle("撮影", forState: .Normal)
+        myButton.layer.cornerRadius = 20.0
+        myButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
+        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+
+        // UIボタンをViewに追加.
+        self.view.addSubview(myButton)
+        
+        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("machinegun", ofType: "mp3")!)
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        audioPlayer = AVAudioPlayer(contentsOfURL: audioPath, error:&audioError)
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            println("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer!.delegate = self
+        audioPlayer!.prepareToPlay()
+        
+        onClickMyButton(myButton)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // ボタンイベント.
+    func onClickMyButton(sender: UIButton){
+        audioPlayer?.play()
+        sleep(3)
+        
+        // ビデオ出力に接続.
+        let myVideoConnection = myImageOutput.connectionWithMediaType(AVMediaTypeVideo)
+        
+        // 接続から画像を取得.
+        self.myImageOutput.captureStillImageAsynchronouslyFromConnection(myVideoConnection, completionHandler: { (imageDataBuffer, error) -> Void in
+
+            // 取得したImageのDataBufferをJpegに変換.
+            let myImageData : NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
+
+            // JpegからUIIMageを作成.
+            let myImage : UIImage = UIImage(data: myImageData)!
+
+            // アルバムに追加.
+            UIImageWriteToSavedPhotosAlbum(myImage, self, nil, nil)
+
+        })
     }
-
-
 }
-
